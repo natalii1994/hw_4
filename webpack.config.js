769aaf5108f5aa1953594b88ee10b088490c9c9f
Natali Kirillova <nata.kirillova1994.com>
2023-02-20 21:04:10 +0300

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: './src/index.js',
@@ -20,4 +22,12 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
+        new HtmlWebpackPlugin({
+            template: './index. html',
+        }),
+    ],
 }
